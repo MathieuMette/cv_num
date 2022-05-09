398c4252e04f81592b2f919e288912bb234a4fba
MathieuMette <mathieu.mette@gmail.com>
2022-05-09 17:33:41 +0200

    nouveau

---
+++ assets/js/new.js
@@ -0,0 +1,27 @@
+var previousButton = document.querySelector('#previousButton');
+var nextButton = document.querySelector('#nextButton');
+var manuRyla = document.querySelector('#manuRyla');
+var btsBac = document.querySelector('#btsBac');
+
+
+nextButton.addEventListener('click', function() {
+    manuRyla.classList.toggle('active');
+    btsBac.classList.toggle('active');
+})
+previousButton.addEventListener('click', function() {
+    manuRyla.classList.toggle('active');
+    btsBac.classList.toggle('active');
+})
+
+
+// Bar nav
+var navBar = document.querySelector('#mm-navBar');
+var navLinks = document.querySelectorAll('.nav-link');
+var navItems = document.querySelectorAll('.nav-item');
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= '630'){
+        navBar.style.opacity = 1;
+    } else if (window.scrollY < '630') {
+        navBar.style.opacity = 0;
+    } 
+})
